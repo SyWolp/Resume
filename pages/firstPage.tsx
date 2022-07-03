c34f8f4fe6997ebaa414060b085438d92020b43d
SyWolp <totqus130@gmail.com>
2022-07-04 06:33:19 +0900

    v1.02

--- pages/firstPage.tsx
+++ pages/firstPage.tsx
@@ -1,9 +1,28 @@
+import { info } from 'console';
 import { useState } from 'react';
+import { useRouter } from "next/router";
 import Header from '../components/header';
 
 export default function FirstPage() {
 
     const [fileImage, setFileImage] = useState("");
+    const [firstInfo, setFirstInfo] = useState([
+        {infoName: "이름", content: ""},
+        {infoName: "나이", content: ""},
+        {infoName: "주소", content: ""},
+    ]);
+    const router = useRouter();
+
+    const saveInfo = (id:number, infoName:any, infoAge:any, infoAdr:any) => {
+        router.push({
+            pathname: `/myresume/${id}`,
+            query: {
+                infoName: infoName, 
+                infoAge: infoAge,
+                infoAdr: infoAdr
+            }
+        }, `/myresume/${id}`);
+    }
 
     async function onImgChange(e:any) {
         if(e.target.files[0]) {
@@ -11,6 +30,13 @@ export default function FirstPage() {
         }
     }
     
+    const valueOnChange = (ch:any, index:number,value:any,set:any) => {
+        const newContent = [...value];
+        console.log(newContent);
+        newContent[index].content = ch;
+        set(newContent);
+    };
+
     return (
         <div className="printPage">
             <Header title="이력서" />
@@ -19,14 +45,20 @@ export default function FirstPage() {
                     {fileImage ? <img src={fileImage} alt="" className="w-32 h-44" /> : null}
                     <input onChange={onImgChange} id="picture" type="file" className="hidden" accept="image/*" />
                 </label>
-                <div>
-                    <div>이름</div>
-                    <div>주소</div>
-                </div>
-                <div>
-                    <div>나이</div>
+                <div className="">
+                    {firstInfo.map((infos,index)=>{
+                        return(
+                            <div key={index}>
+                                <label>
+                                    {infos.infoName}
+                                    <input type="text" value={infos.content}  onChange={(e) => valueOnChange(e.target.value,index,firstInfo,setFirstInfo)} />
+                                </label>
+                            </div>
+                        )
+                    })}
                 </div>
             </header>
+            <button onClick={()=>{saveInfo(1,firstInfo[0].content,firstInfo[1].content,firstInfo[2].content)}}>저장</button>
         </div>
     )
 }
--- pages/myresume/[...id].tsx
+++ pages/myresume/[...id].tsx
@@ -1,7 +1,21 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function My() {
-    return(
-        <div>hi</div>
+export default function Detail() {
+    const router = useRouter();
+    useEffect(() => {
+        if(router.isReady){
+            const { infoName,infoAge,infoAdr } = router.query;
+            console.log(infoName);
+         }
+    }, [router.isReady]);
+    return (
+        <div className="">
+            {router.query.infoName}
+            <br/>
+            {router.query.infoAge}
+            <br/>
+            {router.query.infoAdr}
+        </div>
     )
 }
