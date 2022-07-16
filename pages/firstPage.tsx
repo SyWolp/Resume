cae3572fa9939eee26b0650300847722b766a178
조혜린 <johyelin@johyelin-ui-MacBookPro.local>
2022-07-17 01:48:10 +0900

    v1.03

--- pages/firstPage.tsx
+++ pages/firstPage.tsx
@@ -10,7 +10,7 @@ Amplify.configure(awsExports);
 
 export default function FirstPage() {
 
-    const [fileImage, setFileImage] = useState(".");
+    const [fileImage, setFileImage] = useState("");
     const [firstInfo, setFirstInfo] = useState({
         kname: "",
         ename: "",
@@ -44,11 +44,42 @@ export default function FirstPage() {
                     {fileImage ? <img src={fileImage} alt="" className="w-32 h-44" /> : null}
                     <input onChange={onImgChange} id="picture" type="file" className="hidden" accept="image/*" />
                 </label>
-                <div className="">
-                    <input value={firstInfo.kname} onChange={(e)=>{valueOnChange(e.target.value, "kname")}} type={"text"}></input>
-                    <textarea wrap='hard' value={firstInfo.ename} onChange={(e)=>{valueOnChange(e.target.value, "ename")}} />
+                <div className="flex flex-col w-full pt-8">
+                    <div className='flex-1 flex'>
+                        <label className='flex-1'>
+                            한글 이름
+                            <input className='mx-1 pl-1 border' value={firstInfo.kname} onChange={(e)=>{valueOnChange(e.target.value, "kname")}} type={"text"}></input>
+                        </label>
+                        <label className='flex-1'>
+                            영어 이름
+                            <input className='ml-1 pl-1 border' value={firstInfo.ename} onChange={(e)=>{valueOnChange(e.target.value, "ename")}} type={"text"}></input>
+                        </label>
+                    </div>
+                    <div className='flex-1'>
+                        <label className=''>
+                            주소
+                            <input className='ml-1 pl-1 border w-11/12' value={firstInfo.adr} onChange={(e)=>{valueOnChange(e.target.value, "adr")}} type={"text"}></input>
+                        </label>
+                    </div>
+                    <div className='flex-1 flex'>
+                        <label className='flex-1'>
+                            나이
+                            <input className='ml-1 pl-1 border w-20' value={firstInfo.age} onChange={(e)=>{valueOnChange(e.target.value, "age")}} type={"text"}></input>
+                        </label>
+                        <label className='flex-1'>
+                            성별
+                            <input className='ml-1 pl-1 border w-20' value={firstInfo.gender} onChange={(e)=>{valueOnChange(e.target.value, "gender")}} type={"text"}></input>
+                        </label>
+                        <label className='flex-1'>
+                            핸드폰
+                            <input className='ml-1 pl-1 border w-28' value={firstInfo.cp} onChange={(e)=>{valueOnChange(e.target.value, "cp")}} type={"text"}></input>
+                        </label>
+                    </div>
                 </div>
             </header>
+            <main>
+                
+            </main>
             <button onClick={dataSubmit}>저장</button>
         </div>
     )
